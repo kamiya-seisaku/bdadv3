66afa651e85a70b9631125f5d2d7165c6cda5c34
kkey <spgt417@gmail.com>
2024-06-10 11:31:04 +0900

    key in view3d working yay

--- scripts/ksgame/__main__.py
+++ scripts/ksgame/__main__.py
@@ -184,44 +184,50 @@ class ModalTimerOperator(bpy.types.Operator):
 
         # 2024/6/9 omit old keyhandling for now ##########################
         if event.type == 'ESC':
-                self.cancel(context)
-                return {'CANCELLED'}
+            self.cancel(context)
+            return {'CANCELLED'}
 
         # Todo: need repeated key event handling: pass event while action "brick_hit" is playing in nla (getting better but not perfect)
         # Add and play action "brick_hit" at the scene frame when the bike hits the brick (object distance < threshold)
 
         if event.type in {'A', 'D'}:
-            # Check if the bike is already moving
-            # if moving skip the key event handling
-            # (without imprementing this socond side move happens in the next frame)
-            bike_mover = bpy.data.objects.get('bike-mover')
-            text_obj_key = bpy.data.objects.get('ui.Text.key') # get ui text object for key event capture display
-            text_obj_fn = bpy.data.objects.get('ui.Text.FN') # get ui text object for frame number display
-            # if bike_mover["is_moving"]: # not clear how bike mover custom properties are changing, lets instead use ui_text
-            if text_obj_key.data.body == str(f"bike_mover is moving"):
-                # bike_mover["is_moving"] = False
-                text_obj_key.data.body = str(f"bike_mover is not moving")
-            else:
-                # bike_mover["is_moving"] = True
-                text_obj_key.data.body = str(f"bike_mover is moving")
-                et = event.type
-                frame_number = bpy.context.scene.frame_current
-                # to show the score in the 3D view, the body of the ui text object
-                # is set according to the same object's custom property "score"
-                text_obj_fn.data.body = str(f"FN:{frame_number}")
-                # key event handling
-                if et == 'A':
-                    if bike_mover.location.x < 1:
-                        bike_mover.location.x += 0.5
-                if et == 'D':
-                    if bike_mover.location.x > -1:
-                        bike_mover.location.x -= 0.5
-                bpy.context.view_layer.objects.active = bike_mover #Need this to make location changes into blender data
-                bpy.context.view_layer.update() #Need this for the change to be visible in 3D View
-
-        # self.path_util.update_path_bricks(bpy.context.scene.frame_current)
+            key_input = event.type
+            self.key_handling(context, event, key_input)
+            return {'PASS_THROUGH'}
 
         return {'PASS_THROUGH'}
+
+    def key_handling(self, context, event, key_input):
+        # Check if the bike is already moving
+        # if moving skip the key event handling
+        # (without imprementing this socond side move happens in the next frame)
+        bike_mover = bpy.data.objects.get('bike-mover')
+        text_obj_key = bpy.data.objects.get('ui.Text.key') # get ui text object for key event capture display
+        text_obj_fn = bpy.data.objects.get('ui.Text.FN') # get ui text object for frame number display
+        # if bike_mover["is_moving"]: # not clear how bike mover custom properties are changing, lets instead use ui_text
+        if text_obj_key.data.body == str(f"bike_mover is moving"):
+            # bike_mover["is_moving"] = False
+            text_obj_key.data.body = str(f"bike_mover is not moving")
+        else:
+            # bike_mover["is_moving"] = True
+            text_obj_key.data.body = str(f"bike_mover is moving")
+            et = event.type
+            frame_number = bpy.context.scene.frame_current
+            # to show the score in the 3D view, the body of the ui text object
+            # is set according to the same object's custom property "score"
+            text_obj_fn.data.body = str(f"FN:{frame_number}")
+            # key event handling
+            if et == 'A':
+                if bike_mover.location.x < 1:
+                    bike_mover.location.x += 0.5
+            if et == 'D':
+                if bike_mover.location.x > -1:
+                    bike_mover.location.x -= 0.5
+            bpy.context.view_layer.objects.active = bike_mover #Need this to make location changes into blender data
+            bpy.context.view_layer.update() #Need this for the change to be visible in 3D View
+
+        # self.path_util.update_path_bricks(bpy.context.scene.frame_current)
+        return
 
     def execute(self, context):
         # called when bpy.ops.wm.modal_timer_operator() is called or user selects menu
@@ -263,7 +269,7 @@ class ModalTimerOperator(bpy.types.Operator):
     def cancel(self, context):
         wm = context.window_manager
         bpy.app.handlers.frame_change_post.remove(self.modal)
-
+        return {'PASS_THROUGH'}
 
 #--------------------------------------------
 # Register ModalTimerOperator in layout menu
@@ -280,8 +286,8 @@ def register():
     bpy.types.VIEW3D_MT_view.append(menu_func)
 
 # Todo: comment out [debug codes]
-#register()
-# bpy.ops.wm.modal_timer_operator()
+register()
+bpy.ops.wm.modal_timer_operator()
 #init_bricks()
 #unregister()
 
